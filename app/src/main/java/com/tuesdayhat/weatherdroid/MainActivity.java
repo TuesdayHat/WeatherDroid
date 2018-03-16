2326e264fa241e056841734a7cb6090355971ed9
Student <example@example.com>
2018-03-16 11:21:21 -0700

    add click listeners to mainActivity

--- app/src/main/java/com/tuesdayhat/weatherdroid/MainActivity.java
+++ app/src/main/java/com/tuesdayhat/weatherdroid/MainActivity.java
@@ -1,14 +1,43 @@
 package com.tuesdayhat.weatherdroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.ButterKnife;
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.weatherButton) Button mWeatherButton;
+    @BindView(R.id.aboutButton) Button mAboutButton;
+    @BindView(R.id.weatherLocationInput) EditText mWeatherLocationInput;
+    @BindView(R.id.appTitle) TextView mAppTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        mAboutButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v == mAboutButton){
+            Intent intent = new Intent(MainActivity.this, AboutPage.class);
+            startActivity(intent);
+        } else if (v == mWeatherButton){
+            String location = mWeatherLocationInput.getText().toString();
+            Intent intent = new Intent(MainActivity.this, SourceDetails.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }
