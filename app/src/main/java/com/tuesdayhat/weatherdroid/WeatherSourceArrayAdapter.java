a5d1d9bc600d5cd465ee7988d5b00e128d879732
Student <example@example.com>
2018-03-16 13:21:34 -0700

    create Array Adapter for list of sources

---
+++ app/src/main/java/com/tuesdayhat/weatherdroid/WeatherSourceArrayAdapter.java
@@ -0,0 +1,26 @@
+package com.tuesdayhat.weatherdroid;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class WeatherSourceArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private Object[] mWeatherModels;
+
+    public WeatherSourceArrayAdapter (Context mContext, int resource, String[] mWeatherModels){
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mWeatherModels = mWeatherModels;
+    }
+
+    @Override
+    public Object getItem(int position){
+        Object model = mWeatherModels[position];
+        return String.format("Source: %s", model.toString());
+    }
+
+    @Override
+    public int getCount() {
+        return mWeatherModels.length;
+    }
+}
