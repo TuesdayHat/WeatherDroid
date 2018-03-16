71423b7d76d0f6fa478a15aac3a3b6eaee945cba
Student <example@example.com>
2018-03-16 13:41:01 -0700

    add styling to Source List page

--- app/src/main/java/com/tuesdayhat/weatherdroid/SourceDetails.java
+++ app/src/main/java/com/tuesdayhat/weatherdroid/SourceDetails.java
@@ -1,13 +1,27 @@
 package com.tuesdayhat.weatherdroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.BindView;
 
 public class SourceDetails extends AppCompatActivity {
+    //TODO: convert this to Cardview/RecyclerView based layout.
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_source_details);
+
+
     }
 }
